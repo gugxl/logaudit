ebded1c26efb98a48a69500926075f4a71787e98
guxinlong <guxl@koal.com>
2020-01-04 22:18:38 +0800

    添加几个基础维度的实体类

---
+++ logetl/src/main/java/com/gugu/transformer/model/dim/StatsDimension.java
@@ -0,0 +1,12 @@
+package com.gugu.transformer.model.dim;
+
+import com.gugu.transformer.model.dim.base.BaseDimension;
+
+/**
+ * @author gugu
+ * @Classname StatsDimension
+ * @Description TODO
+ * @Date 2020/1/4 22:13
+ */
+public abstract class StatsDimension extends BaseDimension {
+}
---
+++ logetl/src/main/java/com/gugu/transformer/model/dim/base/BaseDimension.java
@@ -0,0 +1,13 @@
+package com.gugu.transformer.model.dim.base;
+
+import org.apache.hadoop.io.WritableComparable;
+
+/**
+ * @author gugu
+ * @Classname BaseDimension
+ * @Description 顶级的维度信息类
+ * @Date 2020/1/4 21:41
+ */
+public abstract class BaseDimension implements WritableComparable<BaseDimension> {
+    // nothing
+}
